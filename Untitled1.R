a8fc4bfa0c7044dc84307b7005a3295c1e9b3cce
Rob Moore <rob.moore@gmail.com>
2015-12-29 00:04:51 -0600

    Interim commit as writing report.

--- Untitled1.R
+++ Untitled1.R
@@ -1,11 +1,26 @@
 set.seed(19394399)
 
+download.maybe <- function(url, refetch=FALSE, path=".") {
+  dest <- file.path(path, basename(url))
+  if (refetch || !file.exists(dest))
+    download.file(url, dest)
+  dest
+}
+
+download.maybe("http://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip")
+
+if (!file.exists("final")) {
+  unzip("Coursera-SwiftKey.zip")
+}
+# TODO: Update R packages
+
 # install.packages("quanteda", "LaF", "hashr")
 library(LaF) # for sample_lines
 library(quanteda)
-library(hashr)
+#library(hashr)
+library(parallel)
 
-nLinesRatio <- 1e-1
+nLinesRatio <- .6
 nTwitterLines <- 2360148 # determine_nlines("final/en_US/en_US.twitter.txt")
 nBlogLines <- 899288 # determine_nlines("final/en_US/en_US.blogs.txt")
 nNewsLines <- 1010242 # determine_nlines("final/en_US/en_US.news.txt")
@@ -15,18 +30,33 @@ blogs <- sample_lines("final/en_US/en_US.blogs.txt", nBlogLines * nLinesRatio, n
 news <- sample_lines("final/en_US/en_US.news.txt", nNewsLines * nLinesRatio, nNewsLines)
 
 #all <- c(tweets, blogs, news)
-all <- tokenize(c(tweets, blogs, news), what = "sentence", simplify = TRUE)
+all <- tweets
+nsentences <- length(all)
+all <- tokenize(all, what = "sentence", simplify = TRUE)
 #allTokens <- tokenize(toLower(allSentences), removePunc = TRUE, removeNumbers = TRUE)
 
 # replace profanity with UNK tag
 # https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/0fbd315eb2900bb736609ea894b9bde8217b991a/google_twunter_lol
 badWords <- scan("https://raw.githubusercontent.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en", sep = "\n", what = character())
+# TODO: Filter out phrases, just filter on words
+badWords <- badWords[grepl("^\\w+$", badWords, perl = TRUE)]
 #bwExp <- sapply(badWords, function(x) paste0('(\\s?)', x, '\\w*(\\W)'))
 #allMod <- gsub(paste0(bwExp, collapse = "|"), "\\1<UNK>\\2", all, perl = TRUE)
 # try again without expanding out word 
-bwExp <- sapply(badWords, function(x) paste0('\\s?', x, '\\w*\\b'))
+# Only replace whole words
+#bwExp <- paste0(sapply(badWords, function(x) paste0('\\s?', x, '\\w*\\b')), collapse = "|")
+# TODO: Remove URLs /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+bwExp <- paste0(sapply(badWords, function(x) paste0('\\b', x, '\\b')), collapse = "|")
 allMod <- toLower(all)
-allMod <- gsub(paste0(bwExp, collapse = "|"), " _UNK_ ", all, perl = TRUE)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 2
+# Initiate cluster
+cl <- makeCluster(no_cores, type="FORK", renice = 15)
+
+# TODO: Might make this a sapply call so can parallelize
+#allMod <- gsub(paste0(bwExp, collapse = "|"), "_UNK_", allMod, perl = TRUE)
+allMod <- parSapply(cl, allMod, function(x) gsub(bwExp, "_UNK_", x, perl = TRUE))
 
 #tokensAll <- tokenize(allMod, removePunct = TRUE, removeNumbers = TRUE)
 # grep("_UNK_", allMod)
@@ -43,7 +73,7 @@ allMod <- gsub(paste0(bwExp, collapse = "|"), " _UNK_ ", all, perl = TRUE)
 #unigrams <- ngrams(allTokens, 1)
 #sum(ntoken(unigrams))
 #sum(ntype(unigrams))
-unigramsDfm <- dfm(allMod, removePunct = TRUE)
+unigramsDfm <- dfm(allMod, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
 unigramsDfm <- trim(unigramsDfm, minCount = 1)
 
 # add sentence b/e markers
@@ -56,7 +86,8 @@ unigramsDfm <- trim(unigramsDfm, minCount = 1)
 #bigrams <- ngrams(tokensAll, 2)
 #sum(ntoken(bigrams))
 #sum(ntype(bigrams))
-bigramsDfm <- dfm(unname(sapply(allMod, function(x) paste("_S_", x, "_S_"))), ngrams = 2, removePunct = TRUE)
+bigramsAllMod <- unname(sapply(allMod, function(x) paste("_S_", x, "_S_")))
+bigramsDfm <- dfm(bigramsAllMod, ngrams = 2, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
 bigramsDfm <- trim(bigramsDfm, minCount = 2)
 
 # TODO: these aren't being separated into sentences so need to revisit
@@ -66,9 +97,18 @@ bigramsDfm <- trim(bigramsDfm, minCount = 2)
 #trigrams <- ngrams(allMod, ngrams = 3, toLower = TRUE, removePunct = TRUE)
 #sum(ntoken(trigrams))
 #sum(ntype(trigrams))
-trigramsDfm <- dfm(unname(sapply(allMod, function(x) paste("_S_", x, "_S_"))), ngrams = 3, removePunct = TRUE)
+trigramsAllMod <- unname(sapply(bigramsAllMod, function(x) paste("_S_", x, "_S_")))
+trigramsDfm <- dfm(trigramsAllMod, ngrams = 3, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
 trigramsDfm <- trim(trigramsDfm, minCount = 3)
 #features(trigramsDfm)
+
+quardigramsAllMod <- unname(sapply(trigramsAllMod, function(x) paste("_S_", x, "_S_")))
+quadrigramsDfm <- dfm(quardigramsAllMod, ngrams = 4, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+quadrigramsDfm <- trim(quadrigramsDfm, minCount = 4)
+
+quinquegramsAllMod <- unname(sapply(quardigramsAllMod, function(x) paste("_S_", x, "_S_")))
+quinquegramsDfm <- dfm(quinquegramsAllMod, ngrams = 5, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+quinquegramsDfm <- trim(quinquegramsDfm, minCount = 5)
 
 #topfeatures(unigramsDfm)["the"]
 
@@ -92,7 +132,6 @@ trigramsDfm <- trim(trigramsDfm, minCount = 3)
 
 #unigramsFreq <- featureFreq(unigramsDfm)
 #unigramsFreqDfm <- unigramsDfm / sum(ntoken(unigramsDfm))
-nsentences <- length(all)
 ngramProbs <- function(ngram, nMinusOneFreq, nFreq) {
   nMinusOneGram <- gsub("^(.*)_.*$", "\\1", ngram, perl = TRUE)
   nMinusOneProb <- ifelse(nMinusOneGram == "_S_", #if gram starting sentence
@@ -108,19 +147,35 @@ dfmFreq <- function(ngramDfm) {
 unigramsFreq <- dfmFreq(unigramsDfm)
 bigramsFreq <- dfmFreq(bigramsDfm)
 trigramsFreq <- dfmFreq(trigramsDfm)
+quadrigramFreq <- dfmFreq(quadrigramsDfm)
+quinquegramsFreq <- dfmFreq(quinquegramsDfm)
 
 ngramMLE <- function(ngram, nMinusOneFreq, nFreq) {
   nMinusOneGram <- gsub("^(.*)_.*$", "\\1", ngram, perl = TRUE)
-  nMinusOneCount <- ifelse(is.na(nMinusOneFreq[nMinusOneGram]),
-                          nsentences, # likely beginning of sentence, so no unigram probability
+  nMinusOneCount <- ifelse(is.na(nMinusOneFreq[nMinusOneGram]), 
+                          nsentences, # likely beginning of sentence, so no unigram probability IS THIS VALID?
                           nMinusOneFreq[nMinusOneGram])
-  unname(nFreq[ngram] / nMinusOneCount)
+  # could do below by looking up all names for lower order gram and then searching for all upper order with prefix and
+  # multiple. but how to store this? and merge it back together?
+  unname(nFreq[ngram] / nMinusOneCount) # Might be able to do these all at once if you get all cases? so all "i" for  "i <something>"
 }
 
-bigramProbs <- sapply(names(bigramsFreq), function(x) ngramMLE(x, unigramsFreq, bigramsFreq))
-trigramProbs <- sapply(names(trigramsFreq), function(x) ngramMLE(x, bigramsFreq, trigramsFreq))
+# TODO: Maybe only calculate these when asked? That is, don't precalculate? Might tie in with backoff, too.
+#bigramProbs <- sapply(names(bigramsFreq), function(x) ngramMLE(x, unigramsFreq, bigramsFreq))
+bigramProbs <- parSapply(cl, names(bigramsFreq), function(x) ngramMLE(x, unigramsFreq, bigramsFreq))
+trigramProbs <- parSapply(cl, names(trigramsFreq), function(x) ngramMLE(x, bigramsFreq, trigramsFreq))
+quadrigramProbs <- parSapply(cl, names(quadrigramFreq), function(x) ngramMLE(x, trigramsFreq, quadrigramFreq))
+quinquegramProbs <- parSapply(cl, names(quinquegramsFreq), function(x) ngramMLE(x, quadrigramFreq, quinquegramsFreq))
+
+stopCluster(cl)
 
 trigramProbs[grep("^case_of_[^_]*$", names(trigramProbs))]
+quadrigramProbs[grep("^a_case_of_[^_]*$", names(quadrigramProbs))]
+quinquegramProbs[grep("^and_a_case_of_[^_]*$", names(quinquegramProbs))]
+
+#cbind.dfm: combine dfms
+#changeunits: change units "up" or "down" from sentences to words for corpus
+# removeFeatures: remove features from objects -> perhaps profanity?
 
 # To figure probability of sentence, have to multiple each bigram prob together
 
@@ -145,8 +200,34 @@ trigramProbs[grep("^case_of_[^_]*$", names(trigramProbs))]
 
 # stupid backoff
 
-## counts for frequency in docs?
-#docfreq(dz[,1:20])
+#http://www.inside-r.org/packages/cran/stringr/docs/str_replace_all
+
+# TODO: Make function for repeated calls
+# TODO: Maybe only calculate these when asked? That is, don't precalculate? Might tie in with backoff, too.
+#unigramProbs[grep("^of_[^_]*$", names(unigramProbs))]
+# just get most frequent word? -> 'the'
+bigramProbs <- parSapply(cl, names(bigramsFreq), function(x) ngramMLE(x, unigramsFreq, bigramsFreq))
+trigramProbs <- parSapply(cl, names(trigramsFreq), function(x) ngramMLE(x, bigramsFreq, trigramsFreq))
+quadrigramProbs <- parSapply(cl, names(quadrigramFreq), function(x) ngramMLE(x, trigramsFreq, quadrigramFreq))
+quinquegramProbs <- parSapply(cl, names(quinquegramsFreq), function(x) ngramMLE(x, quadrigramFreq, quinquegramsFreq))
+
+#stopCluster(cl)
+
+# Backoff
+# try 5g with 5 words (or match number of words to Ng)
+# recurse to 4g, 3g, 2g chopping off end word at each level
+# add penalty for each backoff
+
+#cbind.dfm: combine dfms
+#changeunits: change units "up" or "down" from sentences to words for corpus
+#removeFeatures: remove features from objects -> perhaps profanity?
+
+# TODO: How to save off model to disk?
+# saveRDS(mod, "mymodel.rds")
+# save(object list,file="myfile.RData")
+
+#freqTerms <- topfeatures(fomcDfm, n = nfeature(fomcDfm))
+#freqTerms[freqTerms >= 2]
 
 # can use textfile to load file
 #myCorpus <- corpus(x)
---
+++ Untitled2.R
@@ -0,0 +1,113 @@
+set.seed(19394399)
+
+download.maybe <- function(url, refetch=FALSE, path=".") {
+  dest <- file.path(path, basename(url))
+  if (refetch || !file.exists(dest))
+    download.file(url, dest)
+  dest
+}
+download.maybe("http://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip")
+
+# unzip file if not previously done
+if (!file.exists("final")) {
+  unzip("Coursera-SwiftKey.zip")
+}
+
+library(LaF) # for sample_lines
+library(quanteda)
+library(parallel)
+
+nLinesRatio <- .6
+nTwitterLines <- 2360148 # can use determine_nlines("final/en_US/en_US.twitter.txt")
+nBlogLines <- 899288 # can use determine_nlines("final/en_US/en_US.blogs.txt")
+nNewsLines <- 1010242 # can use determine_nlines("final/en_US/en_US.news.txt")
+
+tweets <- sample_lines("final/en_US/en_US.twitter.txt", nTwitterLines * nLinesRatio, nTwitterLines)
+blogs <- sample_lines("final/en_US/en_US.blogs.txt", nBlogLines * nLinesRatio, nBlogLines)
+news <- sample_lines("final/en_US/en_US.news.txt", nNewsLines * nLinesRatio, nNewsLines)
+
+all <- c(tweets, blogs, news)
+all <- tokenize(all, what = "sentence", simplify = TRUE)
+nsentences <- length(all)
+
+# replace profanity with UNK tag
+excluded <- scan("https://raw.githubusercontent.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en", 
+                 sep = "\n", 
+                 what = character())
+# Filter out phrases, just use words
+excluded <- excluded[grepl("^\\w+$", excluded, perl = TRUE)]
+# Remove URLs
+#excluded <- c(excluded, "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?)")
+# Only replace whole words
+excluded <- paste0(sapply(excluded, function(x) paste0('\\b', x, '\\b')), collapse = "|")
+# Convert to lower case to ease comparisons
+allMod <- toLower(all)
+
+# Initiate cluster for parallel operations
+cl <- makeCluster(detectCores() - 2, type="FORK", renice = 15)
+# Remove words to be excluded using _UNK_ placeholder
+allMod <- parSapply(cl, allMod, function(x) gsub(excluded, "_UNK_", x, perl = TRUE))
+# Stop cluster as we're done with it
+stopCluster(cl)
+
+# Create document-feature matrices (DFMs) for each ngram type (2-5)
+unigramsDfm <- dfm(allMod, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+unigramsDfm <- trim(unigramsDfm, minCount = 2)
+
+# add sentence begin/end markers
+bigramsAllMod <- unname(sapply(allMod, function(x) paste("_S_", x, "_S_")))
+bigramsDfm <- dfm(bigramsAllMod, ngrams = 2, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+bigramsDfm <- trim(bigramsDfm, minCount = 2)
+
+trigramsAllMod <- unname(sapply(bigramsAllMod, function(x) paste("_S_", x, "_S_")))
+trigramsDfm <- dfm(trigramsAllMod, ngrams = 3, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+trigramsDfm <- trim(trigramsDfm, minCount = 2)
+
+quardigramsAllMod <- unname(sapply(trigramsAllMod, function(x) paste("_S_", x, "_S_")))
+quadrigramsDfm <- dfm(quardigramsAllMod, ngrams = 4, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+quadrigramsDfm <- trim(quadrigramsDfm, minCount = 2)
+
+quinquegramsAllMod <- unname(sapply(quardigramsAllMod, function(x) paste("_S_", x, "_S_")))
+quinquegramsDfm <- dfm(quinquegramsAllMod, ngrams = 5, removePunct = TRUE, toLower = FALSE, removeNumbers = TRUE, removeTwitter = TRUE)
+quinquegramsDfm <- trim(quinquegramsDfm, minCount = 2)
+
+# Extract features with counts from each DFM
+dfmFreq <- function(ngramDfm) {
+  topfeatures(ngramDfm, nfeature(ngramDfm))
+}
+
+unigramsFreq <- dfmFreq(unigramsDfm)
+bigramsFreq <- dfmFreq(bigramsDfm)
+trigramsFreq <- dfmFreq(trigramsDfm)
+quadrigramFreq <- dfmFreq(quadrigramsDfm)
+quinquegramsFreq <- dfmFreq(quinquegramsDfm)
+
+plot(topfeatures(unigramsDfm, 100), log = "y", cex = .6, ylab = "Term frequency")
+unigramsDfmPct <- unigramsFreq * 100
+plot(unigramsDfmPct[1:10], type="b", xlab="Top Ten Words", ylab="Percentage of Full Text", xaxt ="n")
+axis(1, 1:10, labels = names(unigramsDfmPct[1:10]))
+
+# ORIGINAL from above plot
+#plot(topfeatures(unigramsDfm, 100), log = "y", cex = .6, ylab = "Term frequency")
+#myDfmPct <- weight(unigramsDfm, "relFreq") * 100
+#plot(topfeatures(unigramsDfm), type="b",
+#     +      xlab="Top Ten Words", ylab="Percentage of Full Text", xaxt ="n")
+#axis(1, 1:10, labels = names(topfeatures(unigramsDfm)))
+
+#word clouds
+#immigDfm <- dfm(subset(immigCorpus, party=="BNP"), ignoredFeatures = stopwords("english"))
+#plot(immigDfm, random.color = TRUE, rot.per = .25, colors = sample(colors()[2:128], 5))
+#
+#plot(mydfm, max.words=100, colors = brewer.pal(6, "Dark2"), scale=c(8, .5))
+
+#mydfm <- dfm(subset(inaugCorpus, Year>1980))
+#mydfmSW <- dfm(subset(inaugCorpus, Year>1980), ignoredFeatures=stopwords("english"))
+#results <- data.frame(TTR = lexdiv(mydfm, "TTR"),
+#                      CTTR = lexdiv(mydfm, "CTTR"),
+#                      U = lexdiv(mydfm, "U"),
+#                      TTRs = lexdiv(mydfmSW, "TTR"),
+#                      CTTRs = lexdiv(mydfmSW, "CTTR"),
+#                      Us = lexdiv(mydfmSW, "U"))
+#results
+#cor(results)
+#t(lexdiv(mydfmSW, "Maas"))
